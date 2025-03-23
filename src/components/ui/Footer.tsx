5b79d20f29857738fa36d9bc744b0a55bed2d6f8
themaryanjuguna <themaryanjuguna@gmail.com>
2025-03-24 01:41:50 +0300

    Footer link update

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <footer className="mt-8 text-center text-sm text-gray-500 dark:text-gray-400">
             <hr />
-            <p>Just another <a href="https://linktr.ee/usingiziinitiative" target="_blank" rel="noopener noreferrer">#usingiziinitiative Campaign </a>in collaboration with <a href="https://linktr.ee/scatterbrainhub" target="_blank" rel="noopener noreferrer">Scatterbrain Hub</a></p>
+            <p>Just another <a href="https://linktr.ee/usingiziinitiative" target="_blank" rel="noopener noreferrer">#usingiziinitiative</a> Campaign in collaboration with <a href="https://linktr.ee/scatterbrainhub" target="_blank" rel="noopener noreferrer">Scatterbrain Hub</a></p>
         </footer>
     );
 }
